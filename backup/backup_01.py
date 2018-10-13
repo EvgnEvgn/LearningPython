a743d585a16d6c7216febaeee70037156cc6d031
Evgeniy Sharipov <sharipovevgn@gmail.com>
2018-10-13 15:37:44 +0300

    first version backup program

--- backup/backup_01.py
+++ backup/backup_01.py
@@ -1,2 +1,15 @@
-a = 'sad'
-print(a)
+import os
+import time
+
+source = ['"C:\\Only for men"', 'C:\\My_docs']
+
+target_dir = 'G:\\Backup'
+
+target = target_dir + os.sep + time.strftime('%Y%m%d%H%M%S') + '.zip'
+
+zip_command = "zip -qr {0} {1}".format(target, ' '.join(source))
+
+if os.system(zip_command) == 0:
+    print('Резервная копия успешно создана в ', target)
+else:
+    print('Создание резервной копии не удалось')
