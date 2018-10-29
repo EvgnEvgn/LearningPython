ad366248a2ed4f631a29622bde63210085d41375
Евгений <sharipovevgn@gmail.com>
2018-10-29 18:47:16 +0300

    updated multiple regression

--- linear regression/multiple_regression.py
+++ linear regression/multiple_regression.py
@@ -2,17 +2,32 @@ import pandas as pd
 import sklearn.linear_model as lm
 import matplotlib.pyplot as plt
 import numpy as np
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
 
 data = pd.read_csv("MulRegressionData.txt", sep="\t")
 x = data.iloc[:, :-1]
 y = data.iloc[:, -1]
 
 corr = data.corr(method='pearson')
-print()
+print('Correlation coefficients')
 print(corr.to_string())
 
 skm = lm.LinearRegression()
 skm.fit(x, y)
-print("R coef =", skm.coef_)
+print("\nR coef =", skm.coef_)
 print("Intercept =", skm.intercept_)
 
+#----------------------------------------------------------------
+x_ = sm.add_constant(x)
+model = sm.OLS(y, x_)
+results = model.fit()
+#predicted = model.predict()
+#print("Predicted values:")
+#print(predicted)
+#predictions = model.predict(x)
+print('Model params by statsmodels')
+print(results.params)
+print(results.summary())
+
+#print('Predicted values: ', predictions)
--- linear regression/simple_linear_regression.py
+++ linear regression/simple_linear_regression.py
@@ -8,15 +8,22 @@ data_y = pd.read_csv("Y1.txt", sep=" ", header=None)
 data_x = np.array(data_x.iloc[:])
 data_y = np.array(data_y.iloc[:])
 
-#x_test = x.iloc[80:]
-#y_test = y.iloc[80:]
+x_train = data_x[:89]
+y_train = data_y[:89]
+
+x_test = data_x[90:]
+y_test = data_y[90:]
 
 skm = lm.LinearRegression()
-skm.fit(data_x, data_y)
+skm.fit(x_train, y_train)
 print(skm.intercept_, skm.coef_)
-plt.scatter(data_x, data_y, color='blue')
+plt.scatter(x_train, y_train, color='blue')
 
+plt.show()
+predicted = skm.predict(x_test)
 plt.plot(data_x, skm.predict(data_x), color='red', linewidth=1)
+plt.scatter(x_test, predicted, color='red')
+plt.scatter(x_test, y_test, color='blue')
 plt.show()
 x = data_x.reshape((1, -1))
 y = data_y.reshape((1, -1))
