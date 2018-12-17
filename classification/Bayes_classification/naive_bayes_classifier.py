d824f54b05cdfa66841e192fdb74ec6ebf703a79
Evgeniy Sharipov <sharipovevgn@gmail.com>
2018-12-17 11:31:21 +0300

    updated bayes classification

--- classification/Bayes_classification/naive_bayes_classifier.py
+++ classification/Bayes_classification/naive_bayes_classifier.py
@@ -4,10 +4,58 @@ from itertools import repeat
 from itertools import product
 import math
 import time
+from numpy import sin, cos, pi
+from scipy.optimize import leastsq
+import copy
+
+
+def find_boundary(x, y, n, plot_pts=1000):
+    def sines(theta):
+        ans = np.array([sin(i * theta) for i in range(n + 1)])
+        return ans
+
+    def cosines(theta):
+        ans = np.array([cos(i * theta) for i in range(n + 1)])
+        return ans
+
+    def residual(params, x, y):
+        x0 = params[0]
+        y0 = params[1]
+        c = params[2:]
+
+        r_pts = ((x - x0) ** 2 + (y - y0) ** 2) ** 0.5
+
+        thetas = np.arctan2((y - y0), (x - x0))
+        m = np.vstack((sines(thetas), cosines(thetas))).T
+        r_bound = m.dot(c)
+
+        delta = r_pts - r_bound
+        delta[delta > 0] *= 10
+
+        return delta
+
+    # initial guess for x0 and y0
+    x0 = x.mean()
+    y0 = y.mean()
+
+    params = np.zeros(2 + 2 * (n + 1))
+    params[0] = x0
+    params[1] = y0
+    params[2:] += 1000
+
+    popt = leastsq(residual, x0=params, args=(x, y), ftol=1.e-12, xtol=1.e-12)[0]
+
+    thetas = np.linspace(0, 2 * pi, plot_pts)
+    m = np.vstack((sines(thetas), cosines(thetas))).T
+    c = np.array(popt[2:])
+    r_bound = m.dot(c)
+    x_bound = x0 + r_bound * cos(thetas)
+    y_bound = y0 + r_bound * sin(thetas)
+
+    return x_bound, y_bound
 
 
 def plot_samples(sample1, sample2, sample3=None, classes=None, boundary_points=None):
-
     plt.xlabel('Param X')
     plt.ylabel('Param Y')
     plt.scatter(sample1[:, 0], sample1[:, 1], c='blue', marker="+", s=70)
@@ -24,7 +72,7 @@ def plot_samples(sample1, sample2, sample3=None, classes=None, boundary_points=N
             i += 1
 
     if len(boundary_points) > 0:
-        plt.scatter(boundary_points[:, 0], boundary_points[:, 1], c='black')
+        plt.scatter(boundary_points[:, 0], boundary_points[:, 1], c='black', s=10)
 
     plt.show()
 
@@ -48,12 +96,10 @@ def get_columns(matrix):
 
 
 def get_prior_probability(l_y, l):
-
-    return l_y/l
+    return l_y / l
 
 
 def get_rectangular_kernel(z):
-
     return 0.5 * (abs(z) <= 1)
 
 
@@ -77,14 +123,13 @@ def get_columns(array):
 def get_parzen_rozenblatt_probability_estimate(value_of_x_attribute, values_of_x_attribute, h):
     m = len(values_of_x_attribute)
     kernels = [get_kernel(value_of_x_attribute, y_attr, h) for y_attr in values_of_x_attribute]
-    #kernels = list(map(get_kernel, repeat(value_of_x_attribute, m), values_of_x_attribute, repeat(h, m)))
     kernels_sum = sum(kernels)
     probability_estimate = kernels_sum / (m * h)
 
     return probability_estimate
 
 
-def gamma(x_object, array_of_x_objects, array_h, lambda_y, l_y, l):
+def gamma(x_object, array_of_x_objects, h, lambda_y, l_y, l):
     prior_probability = get_prior_probability(l_y, l)
     b = math.log(lambda_y * prior_probability)
 
@@ -93,17 +138,69 @@ def gamma(x_object, array_of_x_objects, array_h, lambda_y, l_y, l):
     probability_estimates = []
     index = 0
     for attribute in x_object:
-        estimate = get_parzen_rozenblatt_probability_estimate(attribute, x_objects[index], array_h[index])
+        estimate = get_parzen_rozenblatt_probability_estimate(attribute, x_objects[index], h)
         index += 1
         probability_estimates.append(estimate)
-    #probability_estimates = list(map(get_parzen_rozenblatt_probability_estimate, x_object, x_objects, array_h))
 
     if any(x == 0.0 for x in probability_estimates):
         return 0.0
-    c = sum([math.log(pe) for pe in probability_estimates])
-    #c = np.sum(list(map(math.log, probability_estimates)))
+    else:
+        c = sum([math.log(pe) for pe in probability_estimates])
 
     return b + c
+
+
+def leave_one_out(classes):
+    start_time = time.process_time()
+
+    h = 0.1
+    h_step = 0.05
+    h_stop = 7
+    lambda_y = 1
+    all_objects = []
+    a_values_for_classes = {}
+
+    for cls, class_objects in classes.items():
+        all_objects.append(class_objects)
+        a_values_for_classes.setdefault(cls, 0.0)
+
+    loo_result = {}
+
+    all_objects = np.concatenate(all_objects)
+    length_of_all_objects = len(all_objects)
+
+    while h <= h_stop:
+        a = []
+        for obj in all_objects:
+            true_class_for_object = ""
+            for cls, class_objects in classes.items():
+                obj = list(obj)
+                objects = class_objects.copy()
+
+                if obj in objects:
+                    true_class_for_object = cls
+                    objects.remove(obj)
+
+                l_y = len(objects)
+                g = gamma(obj, np.array(objects), h, lambda_y, l_y, length_of_all_objects)
+                a_values_for_classes.update({cls: g})
+
+            a_values_for_classes = {k: v for k, v in a_values_for_classes.items() if v != 0.0}
+
+            if len(a_values_for_classes) == 0:
+                a.append(1)
+            elif len(a_values_for_classes) > 1 and len(set(a_values_for_classes.values())) == 1:
+                a.append(1)
+            else:
+                best_class = max(a_values_for_classes, key=a_values_for_classes.get)
+                a.append(0) if best_class == true_class_for_object else a.append(1)
+
+        loo_result.update({h: np.sum(a)/length_of_all_objects})
+        h += h_step
+
+    end_time = time.process_time()
+    print(end_time - start_time)
+    return loo_result
 
 
 def classify(all_objects, classes):
@@ -123,16 +220,14 @@ def classify(all_objects, classes):
     for cls, objects in classes.items():
         result_classes.setdefault(cls, [])
         a_values.setdefault(cls, 0.0)
-        row, col = np.shape(objects)
-        h_ = list(repeat(h_default, col))
-        h.setdefault(cls, h_)
+        h.setdefault(cls, h_default)
         gammas.setdefault(cls, 0.0)
 
     for x in all_objects:
 
         for cl, class_objects in classes.items():
             l_y = len(class_objects)
-            g = gamma(x, class_objects, h.get(cl), lambda_y, l_y, length_of_all_objects)
+            g = gamma(x, np.array(class_objects), h.get(cl), lambda_y, l_y, length_of_all_objects)
             a = {cl: g}
             a_values.update(a)
             gammas.update({cl: abs(g)})
@@ -151,8 +246,6 @@ def classify(all_objects, classes):
         objects_in_classes.append(x)
         result_classes.update({best_class: objects_in_classes})
 
-
-
     end_time = time.process_time()
     print(end_time - start_time)
 
@@ -162,7 +255,7 @@ def classify(all_objects, classes):
 # Исходные данные
 mu_1 = [3.0, 5.0]
 std_1 = np.matrix([[3.0, 0.0], [0.0, 3.0]], dtype=float)
-n = 300
+n = 100
 mu_2 = [10.0, 5.0]
 std_2 = np.matrix([[1.0, 0.0], [0.0, 1.0]], dtype=float)
 
@@ -174,18 +267,24 @@ x2_y1 = samples_1[:, 1]
 
 x1_y2 = samples_2[:, 0]
 x2_y2 = samples_2[:, 1]
-init_classes = {"1": samples_1, "2": samples_2}
-#plot_samples(samples_1, samples_2)
+init_classes = {"1": samples_1.tolist(), "2": samples_2.tolist()}
+# plot_samples(samples_1, samples_2)
 
-#all_points = np.concatenate((samples_1, samples_2), axis=0)
-all_points = get_points(-10, 20, -10, 20, 0.2)
+# all_points = np.concatenate((samples_1, samples_2), axis=0)
+all_points = get_points(-10, 20, -10, 20, 0.4)
 # times = 10
-# while times > 0:
-classified_points, bound_points = classify(all_points, init_classes)
+# # while times > 0:
+# classified_points, bound_points = classify(all_points, init_classes)
+#
+# class1 = np.array(classified_points["2"])
+# x_bound, y_bound = find_boundary(class1[:, 0], class1[:, 1], 2)
+#
+# boundary = np.column_stack((x_bound, y_bound))
+# #boundary = np.array(bound_points)
+#
+# not_determined = np.array(classified_points["not_determined"], dtype=float)
+# plot_samples(np.array(classified_points["2"]), np.array(classified_points["1"]), not_determined, init_classes, boundary)
 
-bound_points = np.array(bound_points)
-bound_points = get_boundary(bound_points)
-
-not_determined = np.array(classified_points["not_determined"], dtype=float)
-plot_samples(np.array(classified_points["2"]), np.array(classified_points["1"]), not_determined, init_classes, bound_points)
-
+loo = leave_one_out(init_classes)
+plt.plot(loo.keys(), loo.values(), lw=2, c='black')
+plt.show()
